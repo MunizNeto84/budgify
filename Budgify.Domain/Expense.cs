35c446de8417583d2db9ec228fcc1d6b27acebaf
Muniz Neto <muniznetto@outlook.com>
2025-09-20 12:03:20 -0300

    feat(domain): add `Income` and `Expense` entities with Ids

---
+++ Budgify.Domain/Expense.cs
@@ -0,0 +1,11 @@
+﻿namespace Budgify.Domain
+{
+    public class Expense
+    {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Category  { get; set; }
+        public string ExpenseType {  get; set; }
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Budgify.Domain/Income.cs
@@ -0,0 +1,11 @@
+﻿namespace Budgify.Domain
+{
+    public class Income
+    {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Category { get; set; }
+        public string IncomeType { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
