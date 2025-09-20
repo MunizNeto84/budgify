882770fd0e6347227e609c391a10fabfaf8dbd8b
Muniz Neto <muniznetto@outlook.com>
2025-09-20 11:40:56 -0300

    feat(domain): add `Account` entity

---
+++ Budgify.Domain/Account.cs
@@ -0,0 +1,8 @@
+﻿namespace Budgify.Domain;
+
+public class Account
+{
+    public string BankName { get; set; }
+    public decimal InitialBalance { get; set; }
+    public bool HasCreditCard { get; set; }
+}
