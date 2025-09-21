2a89110ecc65ccb3b76827a5c685d0d83a7472cf
Muniz Neto <muniznetto@outlook.com>
2025-09-21 12:07:15 -0300

    chore(style): Enforce waring for `unused` variables.

--- Budgify.Domain/Account.cs
+++ Budgify.Domain/Account.cs
@@ -6,4 +6,9 @@ public class Account
     public string BankName { get; set; }
     public decimal InitialBalance { get; set; }
     public bool HasCreditCard { get; set; }
+
+    public void TestMethod()
+    {
+        int unusedVariable = 5; // Esta variável não é usada
+    }
 }
