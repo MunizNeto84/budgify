ae7300d914fdfc7caeeb97574225c8d46e48a8e4
Muniz Neto <muniznetto@outlook.com>
2025-09-24 21:17:39 -0300

    feat(infra): Implement data persistence with `EF Core`

--- Budgify.ConsoleApp/Program.cs
+++ Budgify.ConsoleApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Budgify.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateDefaultBuilder(args);
+
+builder.ConfigureServices((hostContext, services) =>
+{
+    var connectionString = hostContext.Configuration.GetConnectionString("DefaultConnection");
+    services.AddDbContext<BudgifyDbContext>(options => options.UseNpgsql(connectionString));
+});
+
+var host = builder.Build();
+
+Console.WriteLine("Applicação configurada. Pressione enter para sair.");
+Console.ReadLine();
