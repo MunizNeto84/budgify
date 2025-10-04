181b50e24047bb865c06079111d5aa550dfcfe12
Muniz Neto <muniznetto@outlook.com>
2025-10-04 13:01:31 -0300

    feat(app): Implement Add `Income` feature

---
+++ Budgify.Application/IncomeService.cs
@@ -0,0 +1,27 @@
+﻿using Budgify.Domain;
+
+namespace Budgify.Application
+{
+    public class IncomeService : IIncomeService
+    {
+        private readonly IIncomeRepository _incomeRepository;
+
+        public IncomeService (IIncomeRepository incomeRepository)
+        {
+            _incomeRepository = incomeRepository;
+        }
+
+        public void CreateIncome(DateTime date, string category, string incomeType, decimal amount)
+        {
+            var newIncome = new Income
+            {
+                Id = Guid.NewGuid(),
+                Date = date,
+                Category = category,
+                IncomeType = incomeType,
+                Amount = amount
+            };
+            _incomeRepository.Add(newIncome);
+        }
+    }
+}
