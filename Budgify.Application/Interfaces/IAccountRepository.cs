ec2fe8d1a9a54344ebc8a90dd6f0d7bc55b8028c
Muniz Neto <muniznetto@outlook.com>
2025-12-10 12:36:31 -0300

    feat: Implement account `creation` and `listing logic`

---
+++ Budgify.Application/Interfaces/IAccountRepository.cs
@@ -0,0 +1,11 @@
+﻿using Budgify.Domain.Entities;
+
+namespace Budgify.Application.Interfaces
+{
+    public interface IAccountRepository
+    {
+        public void Add(Account account);
+
+        List<Account> GetAll();
+    }
+}
--- Budgify.ConsoleApp/ConsoleUi.cs
+++ Budgify.ConsoleApp/ConsoleUi.cs
@@ -1,7 +1,16 @@
-﻿namespace Budgify.ConsoleApp
+﻿using Budgify.Application.Interfaces;
+
+namespace Budgify.ConsoleApp
 {
     public class ConsoleUi
     {
+        private readonly IAccountService _accountService;
+
+        public ConsoleUi(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
         public void Run()
         {
             int option = -1;
@@ -21,8 +30,7 @@
                 switch (option)
                 {
                     case 1:
-                        Console.WriteLine("Conta");
-                        WaitUser();
+                        HandleAccountMenu();
                         break;
                     case 2:
                         Console.WriteLine("Receita");
@@ -50,6 +58,62 @@
 
         }
 
+        private void HandleAccountMenu()
+        {
+
+            int option = -1;
+            do
+            {
+                ShowAccountMenu();
+                Console.Write("\nDigite uma opção: ");
+                var input = Console.ReadLine()!;
+                if (!int.TryParse(input, out option))
+                {
+                    option = -1;
+                }
+
+                Console.Clear();
+
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Criar conta:");
+                        Console.Write("Banco: ");
+                        string bank = Console.ReadLine()!;
+                        Console.Write("Saldo Inicial: ");
+                        decimal initialAmount = decimal.Parse(Console.ReadLine()!);
+                        _accountService.CreateAccount(bank, initialAmount);
+                        Console.WriteLine("Conta criada!");
+                        WaitUser();
+                        break;
+                    case 2:
+                        Console.WriteLine("Listar conta:");
+                        var accounts = _accountService.GetAllAccounts();
+                        if(accounts.Count == 0)
+                        {
+                            Console.WriteLine("Não existe conta");
+                        } else
+                        {
+                            foreach (var account in accounts)
+                            {
+                                Console.WriteLine($"Conta: {account.Name} | Saldo: {account.Balance:C}");
+                            }
+                        }
+
+                            WaitUser();
+                        break;
+                    case 0:
+                        break;
+                    default :
+                        Console.WriteLine("Opção inválida.");
+                        WaitUser();
+                        break;
+                }
+
+
+            } while (option != 0);
+         }
+
         private void ShowMenu()
         {
             Console.WriteLine("===============================================");
@@ -63,6 +127,16 @@
             Console.WriteLine("===============================================");
         }
 
+        private void ShowAccountMenu()
+        {
+            Console.WriteLine("===============================================");
+            Console.WriteLine("==================== CONTA ====================");
+            Console.WriteLine("===============================================");
+            Console.WriteLine("1 - Criar Conta");
+            Console.WriteLine("2 - Listar Conta(s)\n");
+            Console.WriteLine("0 - Voltar");
+            Console.WriteLine("===============================================");
+        }
         private void WaitUser()
         {
             Console.WriteLine("\nPressione ENTER para voltar ao menu...");
