e190b4b19af5ec5215b23cd32e6d6c1c574174d4
Muniz Neto <muniznetto@outlook.com>
2025-12-26 12:54:05 -0300

    fix: Update FinancialSummaryService to use `real account balance`

--- Budgify.Application/Services/FinancialSummaryService.cs
+++ Budgify.Application/Services/FinancialSummaryService.cs
@@ -7,25 +7,31 @@ namespace Budgify.Application.Services
     {
         public readonly IIncomeRepository _incomeRepository;
         public readonly IExpenseRepository _expenseRepository;
+        public readonly IAccountRepository _accountRepository;
 
-        public FinancialSummaryService(IIncomeRepository incomeRepository, IExpenseRepository expenseRepository)
+        public FinancialSummaryService(IIncomeRepository incomeRepository, IExpenseRepository expenseRepository, IAccountRepository accountRepository)
         {
             _incomeRepository = incomeRepository;
             _expenseRepository = expenseRepository;
+            _accountRepository = accountRepository;
         }
 
         public FinancialSummaryDto GetSummary()
         {
             var incomes = _incomeRepository.GetAll();
             var expenses = _expenseRepository.GetAll();
+            var account = _accountRepository.GetAll();
 
             decimal totalIncome = incomes.Sum(i => i.Amount);
             decimal totalExpense = expenses.Sum(e => e.Amount);
+
+            decimal currentBalance = account.Sum(a => a.Balance);
+
             return new FinancialSummaryDto
             {
                 TotalIncome = totalIncome,
                 TotalExpense = totalExpense,
-                Balance = totalIncome - totalExpense
+                Balance = currentBalance
             };
         }
     }
